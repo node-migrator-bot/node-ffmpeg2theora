79cd093a9413561a09e801d6c62a72740afc3d97
Bob <admin@sharefarm.net>
2011-07-29 18:11:19 -0400

    v0.0.3c

--- test/encode.js
+++ test/encode.js
@@ -1,12 +1,27 @@
 var ffmpeg2theora = new (require('../lib/ffmpeg2theora'))
 
-ffmpeg2theora.batch([ '/home/bob/Downloads/dan.for.mayor.s02e02.hdtv.xvid-2hd.avi', '/home/bob/Downloads/cops.s23e21.readnfo.hdtv.xvid-2hd.avi'], '/home/bob/Downloads/', 'webm', {
-	video : 9,   	//default is 5
-	audio : 9 	//default is 5
+ffmpeg2theora.batch([ '/home/bob/Downloads/dan.for.mayor.s02e02.hdtv.xvid-2hd.avi', '/home/bob/Downloads/cops.s23e21.readnfo.hdtv.xvid-2hd.avi' ], '/home/bob/Downloads/', 'webm', {
+	video : 9, // default is 5
+	audio : 9
+// default is 5
 })
 
-ffmpeg2theora.setThreads(2)//set for the core count of your pc
+ffmpeg2theora.batch([ {
+	source : '/home/bob/Downloads/video1.avi',
+	dest : '/home/bob/downloads/video1'
+},{
+	source : '/home/bob/Downloads/video2.mkv'
+},{
+	source : '/home/bob/Downloads/video3.avi'
+} ], {
+	dest : '/home/bob/downloads',
+	pingTime : 1200,
+	quality : {
+		video : 5,
+		audio : 5
+	},
+	contaners : [ 'mp4', 'webm' ]
+});
 
-setInterval(function() {
-	console.log(ffmpeg2theora.getActive()[0],true,3)
-}, 5500)
+ffmpeg2theora.setThreads(2)// set for the core count of your pc
+
